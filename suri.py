2c427e713853a5d19f662da6e907af3e8fae5dbb
sireesha98 <41771257+sireesha98@users.noreply.github.com>
2018-08-20 14:55:05 +0530

    Create suri.py

---
+++ suri.py
@@ -0,0 +1,14 @@
+# function to print the first m multiple
+# of a number n without using loop.
+def multiple(m, n):
+ 
+    # inserts all elements from n to 
+    # (m * n)+1 incremented by n.
+    a = range(n, (m * n)+1, n)
+     
+    print(*a)
+ 
+# driver code 
+m = 4
+n = 3
+multiple(m, n)
